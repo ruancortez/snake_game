1953b6279b131aff4fca99202ed5964be0222d93
Ruan Cortez <ruanvictorcortez@gmail.com>
2020-09-10 16:54:07 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function criarBG(){
 }
 
 function criarCobrinha(){
-    for(i=0; i < snake.length; i++){
+    for(i=0; i <snake.length; i++){
         context.fillStyle ="green";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
@@ -36,14 +36,22 @@ function update (event){
     if(event.keyCode == 37 && direction != "right") direction ="left";
     if(event.heyCode == 38 && direction != "down") direction = "up";
     if(event.keyCode == 39 && direction != "left") direction = "right";
-    if(event.keyCode == 40 && direction != "up") directin = "down";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
 }
 
 function iniciarJogo(){
+
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+    for(i = 1; i<snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert("Game Over :(");
+        }
+    }
 
     criarBG();
     criarCobrinha();
@@ -57,14 +65,21 @@ function iniciarJogo(){
     if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
 
-    snake.pop();
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop();
+    }
+    else{
+        food.x = Math.floor(Math.random() * 15 + 1) * box,
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
 
     let newHead = {
         x: snakeX,
-        y: snakeY
+        y: snakeY,
     }
 
     snake.unshift(newHead);
 }
+
 let jogo = setInterval(iniciarJogo, 100);
 
